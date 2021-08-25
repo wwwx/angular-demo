1c04822c9f638c5a5013dda060c465753bd4f491
Wang Xu <wangxu1@jiayinfintech.cn>
2021-08-25 15:11:24 +0800

    Update

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -7,12 +7,10 @@ import { Title } from '@angular/platform-browser';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
-    constructor(private title: Title, private el: ElementRef) {
-    }
+    constructor(private title: Title, private el: ElementRef) {}
 
     ngOnInit(): void {
         // Set title
-        // console.log(this.title.getTitle());
+        console.log(this.title.getTitle());
     }
 }
